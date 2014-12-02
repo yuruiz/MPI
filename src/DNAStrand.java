4a952ec8b1dbbd21eacb8ffad5680e4d8080ae1b
siyuwei <a83349201@gmail.com>
2014-12-02 10:54:45 -0500

    KMeans basic

---
+++ src/DNAStrand.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ * @author siyuwei
+ *
+ */
+public class DNAStrand implements KData {
+
+	private String DNA;
+
+	public DNAStrand(String DNA) {
+		this.DNA = DNA;
+	}
+
+	@Override
+	public double distance(Object toCompare) {
+		DNAStrand s = (DNAStrand) toCompare;
+		double dist = 0d;
+		for (int i = 0; i < DNA.length(); i++) {
+			if (DNA.charAt(i) != s.DNA.charAt(i)) {
+				dist++;
+			}
+		}
+		return dist;
+	}
+}
